3d6bd23e53bf1abd516767cfc9c24e95773a196a
Thai Nguyen <trevortknguyen@gmail.com>
2017-01-21 18:07:15 -0600

    Added user interaction test

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message',msg);
-    io.emit('notification', getMockNotif());
+    io.emit('notification', getMockNotif(msg));
   });
 });
 
@@ -33,6 +33,15 @@ function getMockNotif() {
   return {
     date: "2 days ago",
     summary: "Donald Trump became President",
+    imgurl: "http://static4.businessinsider.com/image/56c640526e97c625048b822a-480/donald-trump.jpg",
+    likes: 3
+  };
+}
+
+function getMockNotif(msg) {
+  return {
+    date: "2 days ago",
+    summary: msg,
     imgurl: "http://static4.businessinsider.com/image/56c640526e97c625048b822a-480/donald-trump.jpg",
     likes: 3
   };
