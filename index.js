1ea5a78fdadb3d8e1b4b81120750023429aedc90
Thai Nguyen <trevortknguyen@gmail.com>
2017-01-21 18:03:38 -0600

    Added Angular test

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ app.get('/', function(req, res){
   res.sendFile(__dirname + '/html/index.html');
 });
 
- app.get(/^(.+)$/, function(req, res){ 
-     res.sendFile(__dirname + '/html' + req.params[0]); 
+ app.get(/^(.+)$/, function(req, res){
+     res.sendFile(__dirname + '/html' + req.params[0]);
  });
 
 
@@ -21,9 +21,19 @@ io.on('connection', function(socket){
   socket.on('chat message', function(msg){
     console.log('message: ' + msg);
     io.emit('chat message',msg);
+    io.emit('notification', getMockNotif());
   });
 });
 
 http.listen(port, function(){
   console.log('listening on *:' + port);
 });
+
+function getMockNotif() {
+  return {
+    date: "2 days ago",
+    summary: "Donald Trump became President",
+    imgurl: "http://static4.businessinsider.com/image/56c640526e97c625048b822a-480/donald-trump.jpg",
+    likes: 3
+  };
+}
