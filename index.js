70c1655bab3008b148c70351080974ee25d27d6f
nefaerious <samshin102@gmail.com>
2017-02-02 08:57:50 -0600

    fixes to the database

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var db = require(database);
+var db = require('database.js');
 
 var port = process.env.PORT || 80;
 
