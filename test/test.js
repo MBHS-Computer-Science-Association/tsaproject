39a2bcea0a3a88af4f6a95743df1e6c1f4472782
Thai Nguyen <trevortknguyen@users.noreply.github.com>
2017-02-05 19:23:03 -0600

    Add testing framework (#33)
    * Set up testing
    
    * Change to port that does not require sudo
    
    * Add TravisCI status to README
    
    * Add istanbul for coverage

---
+++ test/test.js
@@ -0,0 +1,7 @@
+var assert = require('chai').assert;
+
+describe('PostgreSQL Database', function() {
+    describe('get users function', function() {
+        it('should return all of the users in json');
+    });
+});
