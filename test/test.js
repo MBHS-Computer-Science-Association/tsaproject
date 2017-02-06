9131d5022ccaf1277f3fadf863c8022e0795a4a3
Thai Nguyen <trevortknguyen@gmail.com>
2017-02-06 09:13:07 -0600

    Add database tests

--- test/test.js
+++ test/test.js
@@ -1,7 +1,35 @@
 var assert = require('chai').assert;
 
 describe('PostgreSQL Database', function() {
-    describe('get users function', function() {
-        it('should return all of the users in json');
-    });
+  describe('get users function', function() {
+    it('should return all of the users in json');
+  });
+
+  describe('insert user function', function() {
+    it('should insert user properly');
+  });
+
+  describe('get groups function', function() {
+    it('should return all of the groups in json');
+  });
+
+  describe('insert group function', function() {
+    it('should insert group properly');
+  });
+
+  describe('get messages function', function() {
+    it('should return all of the messages in json');
+  });
+
+  describe('insert message function', function() {
+    it('should insert message properly');
+  });
+
+  describe('get announcements function', function() {
+    it('should return all of the announcements in json');
+  });
+
+  describe('insert announcement function', function() {
+    it('should insert announcement properly');
+  });
 });
