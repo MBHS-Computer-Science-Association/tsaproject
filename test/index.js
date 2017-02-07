222a4a32f68b14a74caf8b1654ac0f4dc3b3f2ab
Thai Nguyen <trevortknguyen@gmail.com>
2017-02-07 09:00:14 -0600

    Add main application test case

---
+++ test/index.js
@@ -0,0 +1,9 @@
+var assert = require('chai').assert;
+
+var application = require('../index.js');
+
+describe('application', function() {
+  describe('users', function() {
+    it('should manage the users properly');
+  });
+});
