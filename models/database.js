5f271d58a49362ec6b3c754933327dda95909176
nefaerious <samshin102@gmail.com>
2017-02-07 11:50:20 -0600

    changes for the better

--- models/database.js
+++ models/database.js
@@ -1,7 +1,7 @@
 var exports = module.exports = {};
 var url = require('url');
+var Pool = require('pg-pool');
 
-const pg = require('pg');
 
 var params = url.parse(process.env.DATABASE_URL || 'postgres://postgres:password@localhost:5432/todo');
 var auth = params.auth.split(':');
@@ -16,73 +16,4 @@ var config = {
   idleTimeoutMillis: 30000
 };
 
-var pool = new pg.Pool(config);
-
-function database(){
-  pool.connect(function (err, client, done){
-    if (err) throw err;
-    client.query('CREATE TABLE IF NOT EXISTS Users(name VARCHAR(64) NOT NULL, pin VARCHAR(4) NOT NULL, admin BOOLEAN NOT NULL, status BOOLEAN NOT NULL, userID INT PRIMARY KEY NOT NULL)', function(err, client, done) {
-      if(err){
-        throw err;
-      }
-    });
-    client.query('CREATE TABLE IF NOT EXISTS Groups(name VARCHAR(64) NOT NULL, userID INT NOT NULL, messageID INT NOT NULL, annoucementID INT NOT NULL, groupID INT PRIMARY KEY NOT NULL)', function(err, client, done) {
-      if(err){
-        throw err;
-      }
-    });
-    //add date later
-    client.query('CREATE TABLE IF NOT EXISTS Messages(message varchar(256) NOT NULL, userID INT NOT NULL, messageID INT PRIMARY KEY NOT NULL)', function(err, client, done) {
-      if(err){
-        throw err;
-      }
-    });
-    client.query('CREATE TABLE IF NOT EXISTS Annoucements(name VARCHAR(256) NOT NULL, userID INT NOT NULL, annoucementID INT PRIMARY KEY NOT NULL)', function(err, client, done) {
-      if(err){
-        throw err;
-      }
-    });
-
-  });
-}
-database();
-
-exports.insertData = function (param){
-  pool.connect(function(err, client, done) {
-    if(err) {
-      done();
-      return console.error('Error fetching client from pool', err);
-    }
-    const query = client.query(param);
-    query.on('end', () => { done(); });
-  });
-}
-
-exports.readData = function(param){
-  var results = []
-  pool.connect(function(err, client, done) {
-    if(err) {
-      done();
-      return console.error('Error fetching client from pool', err);
-    }
-    //'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)'
-    const query = client.query(param);
-    query.on('row', (row) => {
-      results.push(row);
-    });
-    query.on('end', () => { done(); });
-  });
-  return results;
-}
-
-exports.udData = function(param){
-  pool.connect(function(err, client, done) {
-    if(err) {
-      done();
-      return console.error('Error fetching client from pool', err);
-    }
-    //'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)'
-    const query = client.query(param);
-    query.on('end', () => { done(); });
-  });
-}
+const pool = new Pool(config);
