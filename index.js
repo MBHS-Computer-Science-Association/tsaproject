9d7f537109e0bcb06abeb2955b86d1e52b14facb
SovietBeans <nicholasroth13@gmail.com>
2017-02-13 09:03:59 -0600

    install postgres and node bat

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
-var port = process.env.PORT || 80;
+var port = process.env.PORT || 8000;
 
 app.get(/^(.+)$/, function(req, res){
 	res.sendFile(__dirname + '/html' + req.params[0]);
