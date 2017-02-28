5269a063a95e9dab21997ce8dbfdb3dcccce3eae
nefaerious <samshin102@gmail.com>
2017-02-27 20:46:51 -0600

    new database

--- models/database.js
+++ models/database.js
@@ -1,7 +1,6 @@
 var exports = module.exports = {};
 var url = require('url');
-var Pool = require('pg-pool');
-
+var pg = require('pg');
 
 var params = url.parse(process.env.DATABASE_URL || 'postgres://postgres:password@localhost:5432/todo');
 var auth = params.auth.split(':');
@@ -15,94 +14,27 @@ var config = {
   max: 10,
   idleTimeoutMillis: 30000
 };
-const pool = new Pool(config);
+
+var db = new pg.Client(config);
 
 function database(){
-
-  pool.connect().then(client => {
-    return client.query('CREATE TABLE IF NOT EXISTS Users(info json NOT NULL)').then(() => client);
-  }).then(client => client.release());
-  pool.connect().then(client => {
-    return client.query('CREATE TABLE IF NOT EXISTS Messages(info json NOT NULL)').then(() => client);
-  }).then(client => client.release());
-  pool.connect().then(client => {
-    return client.query('CREATE TABLE IF NOT EXISTS Groups(info json NOT NULL)').then(() => client);
-  }).then(client => client.release());
-  pool.connect().then(client => {
-    return client.query('CREATE TABLE IF NOT EXISTS Annoucements(info json NOT NULL)').then(() => client);
-  }).then(client => client.release());
+  db.connect(function(err) {
+    if (err) throw err;
+    console.log('PostgreSQL is connected!');
+    db.query('CREATE TABLE IF NOT EXISTS users(info json NOT NULL)');
+    db.query('CREATE TABLE IF NOT EXISTS messages(info json NOT NULL)');
+    db.query('CREATE TABLE IF NOT EXISTS groups(info json NOT NULL)');
+    db.query('CREATE TABLE IF NOT EXISTS announcements(info json NOT NULL)');
+  });
 }
 database();
 
-exports.queryDB = function(value){
-  pool.connect().then(client => {
-    return client.query({text: '$1', values: [value]}).then(() => client);
-  }).then(client => client.release());
-  console.log('this worked');
-};
-/*
-    json data
-*/
-exports.insertIntoUsers = function(value){
-  pool.connect().then(client => {
-    return client.query({text: 'INSERT INTO Users(info) VALUES($1);', values: [value]}).then(() => client);
-  }).then(client => client.release());
-};
-/*
-    json data
-*/
-exports.insertIntoGroups = function(row, value){
-  pool.connect().then(client => {
-    return client.query({text: 'INSERT INTO Groups(info) VALUES($1);', values: [value]}).then(() => client);
-  }).then(client => client.release());
-};
-/*
-    json data
-*/
-exports.insertIntoMessages = function(value){
-  pool.connect().then(client => {
-    return client.query({text: 'INSERT INTO Messages(info) VALUES($1);', values: [value]}).then(() => client);
-  }).then(client => client.release());
-};
-/*
-    json data
-*/
-exports.insertIntoAnnouncements = function(value){
-  pool.connect().then(client => {
-    return client.query({text: 'INSERT INTO Announcements(info) VALUES($1);', values: [value]}).then(() => client);
-  }).then(client => client.release());
-};
-
-//only information ever to be updated is user status, function to update user status
-exports.updateUserStatus = function(newValue, user){
-  pool.connect().then(client => {
-    return client.query({text: 'UPDATE Users SET $1 WHERE info#>>\'status\' AND info#>>\'name\'=$2', values: [newValue, user]}).then(() => client);
-  }).then(client => client.release());
-};
-
-
-
-exports.retrieveUsers = function(object, val){
-  console.log('yes');
-  pool.connect().then(client => {
-    return client.query({text: 'SELECT info -> \'$1\' AS $2 FROM Users;', values: [object, val]
-  }).then(() => client).then(res => {
-    console.log(res.rows[0]); console.log('hi')
-  });
-  }).then(client => client.release());
-};
-exports.retrieveGroups = function(toBeSelected, jsonPathway){
-  pool.connect().then(client => {
-    return client.query({text: 'SELECT $1#>>\'{$2}\' AS FROM Groups;', values: [toBeSelected, jsonPathway]}).then(() => client);
-  }).then(client => client.release());
-};
-exports.retrieveMessages = function(toBeSelected, jsonPathway){
-  pool.connect().then(client => {
-    return client.query({text: 'SELECT $1#>>\'{$2}\' AS FROM Messages;', values: [toBeSelected, jsonPathway]}).then(() => client);
-  }).then(client => client.release());
-};
-exports.retrieveAnnouncements = function(toBeSelected, jsonPathway, table){
-  pool.connect().then(client => {
-    return client.query({text: 'SELECT $1#>>\'{$2}\' AS FROM Annoucements;', values: [toBeSelected, jsonPathway]}).then(() => client);
-  }).then(client => client.release());
-};
+exports.addUser = function(array){
+  db.query("INSERT INTO users(info) VALUES($1)", [JSON.stringify(array)]);
+}
+exports.getAllUsers = function(array){
+  var query = db.query('SELECT * FROM users');
+    query.on('row', function(row, result) {
+        rows.unshift(row.data);
+    });
+}
