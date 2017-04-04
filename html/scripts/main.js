0de92047642ee1c4e2979f503488c1ed4ea596aa
Zelanias <Joshua5942@gmail.com>
2017-04-04 08:51:16 -0500

    masking fix attempt 2
    2

--- html/scripts/main.js
+++ html/scripts/main.js
@@ -7,7 +7,7 @@ var oc = 0 //temp old length of group list
 var nc = groupList.length; //temp new length of group listl
 
 var thisUser = {id: 0, nick: "Bismarck", pass: "password", status: "online"};
-getNewUser("Default", "hunter2");
+createNewUser("Default", "hunter2");
 
 socket.emit('getGroups', function(groups){
 	// puts initial groups down
